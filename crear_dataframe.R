dc0a71c4eaca860b72d79aad0ecbf3859c60a195
cristhian <cristhian.ipanaque@gmail.com>
2023-01-16 19:35:29 -0500

    agrege a frescia a la tabla

--- crear_dataframe.R
+++ crear_dataframe.R
@@ -1,5 +1,5 @@
 library(dplyr)
 
 # Crear un dataframe con dos columnas: 'nombre' y 'edad'
-df <- tibble(nombre = c("Juan", "Maria", "Pedro","cristhian"), edad = c(25, 32, 19,29))
+df <- tibble(nombre = c("Juan", "Maria", "Pedro","cristhian","frescia"), edad = c(25, 32, 19, 29, 29))
 df
